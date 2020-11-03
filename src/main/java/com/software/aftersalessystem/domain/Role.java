5265745549366e13ca8039eabec4a6046be6598f
huan <huan.l@outlook.com>
2020-11-03 16:18:20 +0800

    提交权限实体类

---
+++ src/main/java/com/software/aftersalessystem/domain/Role.java
@@ -0,0 +1,10 @@
+package com.software.aftersalessystem.domain;
+
+/**
+ * @Classname Role
+ * @Description TODO
+ * @Date 2020/11/3 16:15
+ * @Created by Huan
+ */
+public class Role {
+}
---
+++ src/main/java/com/software/aftersalessystem/domain/User.java
@@ -0,0 +1,10 @@
+package com.software.aftersalessystem.domain;
+
+/**
+ * @Classname User
+ * @Description TODO
+ * @Date 2020/11/3 16:15
+ * @Created by Huan
+ */
+public class User {
+}
