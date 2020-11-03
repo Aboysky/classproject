42bf7785077ffaedade39685d11208d02ed596ef
huan <huan.l@outlook.com>
2020-11-03 16:12:40 +0800

    commit

---
+++ src/main/java/com/software/aftersalessystem/AftersalessystemApplication.java
@@ -0,0 +1,13 @@
+package com.software.aftersalessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftersalessystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftersalessystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/software/aftersalessystem/AftersalessystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.aftersalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftersalessystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
