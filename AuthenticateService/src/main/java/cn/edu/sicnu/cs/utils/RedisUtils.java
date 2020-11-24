52127a2ca196028f955142ef39ab32fb9f14df78
huan <huan.l@outlook.com>
2020-11-24 20:00:54 +0800

    提交redisutils

--- AuthenticateService/src/main/java/cn/edu/sicnu/cs/utils/RedisUtils.java
+++ AuthenticateService/src/main/java/cn/edu/sicnu/cs/utils/RedisUtils.java
@@ -1,10 +1,19 @@
 package cn.edu.sicnu.cs.utils;
 
+import cn.edu.sicnu.cs.model.Metaoperation;
+import cn.edu.sicnu.cs.service.MetaOperationService;
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.stereotype.Component;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Classname RedisUtils
@@ -18,7 +27,21 @@ public class RedisUtils {
     @Autowired
     static RedisTemplate redisTemplate;
 
+    @Autowired
+    static MetaOperationService metaOperationService;
+
     public static void delete(String key){
         redisTemplate.delete(key);
+    }
+
+    public static void addConfigrationPermissions(){
+        Collection<ConfigAttribute> configAttributes=new ArrayList<>();
+        List<Metaoperation> operations = metaOperationService.selectAll();
+        for (Metaoperation operation:operations) {
+            ConfigAttribute configAttribute=new SecurityConfig(operation.getMolurl()+" "+operation.getMomethod());
+            configAttributes.add(configAttribute);
+        }
+        //将权限存入redis
+        redisTemplate.opsForValue().set("configAttributes:permissions", JSON.toJSONString(operations),480, TimeUnit.MINUTES);
     }
 }
